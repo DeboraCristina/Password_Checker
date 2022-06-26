e9def604362ce9725004322067921da3513ccf56
Debby <debby@pop-os.localdomain>
2022-06-26 00:48:27 -0300

    checker password - shell version

---
+++ colors.py
@@ -0,0 +1,27 @@
+def BLACK():
+    return "\033[1;90m"
+
+def RED():
+    return "\033[1;91m"
+
+def GREEN():
+    return "\033[1;92m"
+
+def YELLOW():
+    return "\033[1;93m"
+
+def BLUE():
+    return "\033[1;94m"
+
+def PURPLE():
+    return "\033[1;95m"
+
+def CYAN():
+    return "\033[1;96m"
+
+def WHITE():
+    return "\033[1;97m"
+
+def NC():
+    return "\033[0m"
+
--- verify.py
+++ verify.py
@@ -1,20 +1,71 @@
-from sys import *
+from sys import argv
+from colors import *
 
-def verify_repeat(a = ""):
-    for i in range(len(a)):
-        for c in range(len(a)):
-            if c != i and a[i] == a[c]:
-                return 0
-    return 1
+def check_pw(s = ''):
+    status = []
+    if len(s) < 10:
+        status.append('w')
+    length = len(s) - 1
 
-def check_uppercase(s = ""):
+    c = 0
+    for u in s:
+        if u.isupper():
+            break
+        if c == length and not u.isupper():
+            status.append('u')
+        c += 1
+
+    c = 0
     for l in s:
-        if l not in "a"
+        if l.islower():
+            break
+        if c == length and not l.islower():
+            status.append('l')
+        c += 1
 
-def check_lowercase(s = ""):
+    c = 0
+    for d in s:
+        if d.isdigit():
+            break
+        if c == length and not d.isdigit():
+            status.append('d')
+        c += 1
+    return status
+
+def show_error_messages(errors = [], l = 0):
+    for error in errors:
+        if error == 'w':
+            print(f"Short Password. {l} length.")
+        elif error == 'u':
+            print("No upperrorcase character.")
+        elif error == 'l':
+            print("No lowerrorcase character.")
+        elif error == 'd':
+            print("No digit.")
+
+def checker(password = ''):
+    status = check_pw(password)
+    if not len(status):
+        print(f"{GREEN()}OK - \"{password}\"{NC()}")
+    else:
+        print(f"{RED()}KO - \"{password}\"")
+        show_error_messages(status, len(password))
+        print(f'{NC()}', end = '')
 
 if len(argv) > 1:
-    if len(argv[1]) >= 10 and verify_repeat(argv[1]):
-        print(f"\033[1;92m{argv[1]} - OK    {len(argv[1])}\033[0m")
-    else:
-        print(f"\033[1;91m{argv[1]} - KO    {len(argv[1])}\033[0m")
+    checker(argv[1])
+
+else:
+    print(f"{PURPLE()}Welcome to Password Checker!{NC()}")
+    while True:
+        print('\n')
+        print(f"{BLUE()}What do you want to do?")
+        print()
+        print(f"(c) Check Password.\n(e) Exit.{NC()}")
+        op = input(f"{WHITE()}: ")
+        print(f"{NC()}", end = '')
+        if op.lower() == 'c':
+            checker(input(f"{WHITE()}Password: "))
+        else:
+            break
+        print(f"{NC()}", end = '')
